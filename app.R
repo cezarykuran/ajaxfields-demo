1b6bb956863854b2b7ad090ad59285c636a3706d
Cezary Kuran <cezary.kuran@gmail.com>
2020-06-15 17:21:15 +0200

    upgrade to new package

--- app.R
+++ app.R
@@ -13,15 +13,17 @@ ui <- fixedPage(
   div(
     titlePanel("NCBI (ajaxfields) test application"),
     div(class='row',
-      div(class='col-md-6', div(ajaxfields::draw('ncbi', 'https://ncbi.dafne.prv.ovh', 'NCBI'))),
-      div(class='col-md-6', div(ajaxfields::draw('ncbi2', 'https://ncbi.dafne.prv.ovh', 'NCBI II')))
+      div(class='col-md-6', div(ajaxfields::draw('ncbi', 'simple'))),
+      div(class='col-md-6', div(ajaxfields::draw('ncbi2', 'elasticsearch')))
     )
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   ncbiData <- callModule(ajaxfields::observer, "ncbi")
+  ajaxfields::loadEngine(session, 'ncbi', 'simple', 'https://ncbi.dafne.prv.ovh')
   ncbi2Data <- callModule(ajaxfields::observer, "ncbi2")
+  ajaxfields::loadEngine(session, 'ncbi2', 'es', 'http://lucene.dafne.prv.ovh/ncbi/')
   
   observe({
     dput(ncbiData())
